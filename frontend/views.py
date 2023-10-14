574d21deafb8d9d750f291eaed9d5ea6de8fcea0
billingsandrewjohnlaptop <billingsandrewjohn@gmail.com>
2023-10-14 01:11:08 -0400

    d

--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,20 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 import sys
-import sys
+import os
 
-import sys
-sys.path.append('C:/Stocks2/backend/Match.py')
+# Get the absolute path of the directory containing the current script (frontend/views.py)
+current_script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the absolute path to the backend directory
+backend_directory_path = os.path.abspath(os.path.join(current_script_dir, os.pardir, 'Stocks2', 'backend'))
+
+# Add the backend directory to the Python path
+sys.path.append('C:/Stocks2/backend')
+
+# Now you can import Match
 import Match
+
+# Use functions or variables from Match
 admin = "dog1"
 adminpswd = "dog1"
 
